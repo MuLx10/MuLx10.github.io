0b0674c262d881e1d0f57ea1c1e76819318f2492
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2018-07-01 20:36:02 +0530

    unable 2 added complete home

--- js/about.js
+++ js/about.js
@@ -37,5 +37,5 @@ $(document).ready(function(){
   // Now to start autoTyping just call the autoType function with the 
   // class of outer div
   // The second paramter is the speed between each letter is typed.   
-  autoType(".type-js",200);
+  autoType(".type-js",20);
 });
